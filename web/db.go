a92d7349aa1e7e2c0d24ce200bcdd8858850fba9
jerry <smg@sightp.com>
2020-04-24 17:35:17 +0800

    优化

--- web/db.go
+++ web/db.go
@@ -2,18 +2,17 @@ package web
 
 import (
 	"encoding/json"
-	"errors"
 
 	"github.com/gentwolf-shen/db-client-api/auth"
 	"github.com/gentwolf-shen/db-client-api/entity"
-
-	"github.com/gentwolf-shen/gohelper/httpclient"
+	"github.com/gentwolf-shen/gohelper/gohttp"
 )
 
 type Db struct {
 	dbServer  string
 	appKey    string
 	appSecret string
+	expires   int64
 }
 
 func New(dbServer, appKey, appSecret string) *Db {
@@ -21,20 +20,23 @@ func New(dbServer, appKey, appSecret string) *Db {
 		dbServer:  dbServer,
 		appKey:    appKey,
 		appSecret: appSecret,
+		expires:   600,
 	}
+}
+
+func (this *Db) Set(expires int64) {
+	this.expires = expires
 }
 
 /**
 查询数据，返回多条记录
 */
-func (this Db) Query(item *entity.SqlMessage) ([]map[string]string, error) {
-	b, err := this.send("/query", item)
+func (this *Db) Query(item *entity.SqlMessage) ([]map[string]string, error) {
+	var rows []map[string]string
+	err := this.send("/query", item, &rows)
 	if err != nil {
 		return nil, err
 	}
-
-	var rows []map[string]string
-	err = json.Unmarshal(b, &rows)
 
 	return rows, err
 }
@@ -42,7 +44,7 @@ func (this Db) Query(item *entity.SqlMessage) ([]map[string]string, error) {
 /**
 查询数据，单条记录
 */
-func (this Db) QueryRow(item *entity.SqlMessage) (map[string]string, error) {
+func (this *Db) QueryRow(item *entity.SqlMessage) (map[string]string, error) {
 	rows, err := this.Query(item)
 	if err != nil {
 		return nil, err
@@ -58,7 +60,7 @@ func (this Db) QueryRow(item *entity.SqlMessage) (map[string]string, error) {
 /**
 查询记录，一个字段
 */
-func (this Db) QueryScalar(item *entity.SqlMessage) (string, error) {
+func (this *Db) QueryScalar(item *entity.SqlMessage) (string, error) {
 	row, err := this.QueryRow(item)
 	if err != nil {
 		return "", err
@@ -79,14 +81,11 @@ func (this Db) QueryScalar(item *entity.SqlMessage) (string, error) {
 /**
 更新数据
 */
-func (this Db) Update(item *entity.SqlMessage) (int64, error) {
-	b, err := this.send("/update", item)
-	if err != nil {
-		return 0, err
-	}
-
+func (this *Db) Update(item *entity.SqlMessage) (int64, error) {
 	rs := &entity.UpdateResult{}
-	if err := json.Unmarshal(b, rs); err != nil {
+
+	err := this.send("/update", item, rs)
+	if err != nil {
 		return 0, err
 	}
 
@@ -96,21 +95,18 @@ func (this Db) Update(item *entity.SqlMessage) (int64, error) {
 /**
 删除数据
 */
-func (this Db) Delete(item *entity.SqlMessage) (int64, error) {
+func (this *Db) Delete(item *entity.SqlMessage) (int64, error) {
 	return this.Update(item)
 }
 
 /**
 添加数据
 */
-func (this Db) Insert(item *entity.SqlMessage) (int64, error) {
-	b, err := this.send("/insert", item)
-	if err != nil {
-		return 0, err
-	}
-
+func (this *Db) Insert(item *entity.SqlMessage) (int64, error) {
 	rs := &entity.InsertResult{}
-	if err := json.Unmarshal(b, rs); err != nil {
+
+	err := this.send("/insert", item, rs)
+	if err != nil {
 		return 0, err
 	}
 
@@ -121,8 +117,8 @@ func (this Db) Insert(item *entity.SqlMessage) (int64, error) {
 事务处理，多条SQL必须是操作同一数据库
 SQL: UPDATE、INSERT、DELETE
 */
-func (this Db) TransactionV1(items []*entity.SqlMessage) (bool, error) {
-	if _, err := this.send("/v1/transaction", items); err != nil {
+func (this *Db) TransactionV1(items []*entity.SqlMessage) (bool, error) {
+	if err := this.send("/v1/transaction", items, nil); err != nil {
 		return false, err
 	}
 
@@ -133,8 +129,8 @@ func (this Db) TransactionV1(items []*entity.SqlMessage) (bool, error) {
 事务处理，多条SQL必须是操作同一数据库
 SQL: UPDATE、INSERT、DELETE
 */
-func (this Db) TransactionV2(items *entity.BatchSqlMessage) (bool, error) {
-	if _, err := this.send("/v2/transaction", items); err != nil {
+func (this *Db) Transaction(items *entity.BatchSqlMessage) (bool, error) {
+	if err := this.send("/v2/transaction", items, nil); err != nil {
 		return false, err
 	}
 
@@ -144,14 +140,13 @@ func (this Db) TransactionV2(items *entity.BatchSqlMessage) (bool, error) {
 /**
 批量查询
 */
-func (this Db) BatchQuery(items []*entity.SqlMessage) ([][]map[string]string, error) {
-	b, err := this.send("/batch/query", items)
+func (this *Db) BatchQuery(items []*entity.SqlMessage) ([][]map[string]string, error) {
+	var rows [][]map[string]string
+
+	err := this.send("/batch/query", items, &rows)
 	if err != nil {
 		return nil, err
 	}
-
-	var rows [][]map[string]string
-	err = json.Unmarshal(b, &rows)
 
 	return rows, err
 }
@@ -159,10 +154,10 @@ func (this Db) BatchQuery(items []*entity.SqlMessage) ([][]map[string]string, er
 /**
 发送数据操作命令
 */
-func (this Db) send(method string, item interface{}) ([]byte, error) {
+func (this *Db) send(method string, item interface{}, v interface{}) error {
 	token, err := auth.GetToken(this.appKey, this.appSecret)
 	if err != nil {
-		return nil, err
+		return err
 	}
 
 	headers := make(map[string]string, 3)
@@ -171,13 +166,18 @@ func (this Db) send(method string, item interface{}) ([]byte, error) {
 
 	b, _ := json.Marshal(item)
 
-	body, err := httpclient.PostToBody(this.dbServer+method, b, headers)
+	_, err = gohttp.Post(this.dbServer+method).
+		SetHeader("Content-Type", "application/json").
+		SetBodyJson(item).
+		BindResponseJson(v).
+		Do()
 	if err != nil {
-		if len(body) == 0 {
-			return nil, err
-		}
-		return nil, errors.New(string(body))
+		return nil
 	}
 
-	return body, nil
+	if err := json.Unmarshal(b, v); err != nil {
+		return err
+	}
+
+	return nil
 }
